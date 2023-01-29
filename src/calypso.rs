91efd9c77ed11a6c4fb48a975fc1236ccf2ee5fd
Liam Kinne <liam@kinne.io>
2023-01-29 22:56:16 +1000

    shorten command names

--- src/calypso.rs
+++ src/calypso.rs
@@ -13,37 +13,37 @@ where
         Calypso { serial }
     }
 
-    pub fn command_start(&mut self) -> Result<(), &'static str> {
-        self.write_command("start");
+    pub fn start(&mut self) -> Result<(), &'static str> {
+        self.command("start");
 
         Ok(())
     }
 
-    pub fn command_stop(&mut self) -> Result<(), &'static str> {
-        self.write_command("stop");
+    pub fn stop(&mut self) -> Result<(), &'static str> {
+        self.command("stop");
 
         Ok(())
     }
 
-    pub fn command_test(&mut self) -> Result<(), &'static str> {
-        self.write_command("test");
+    pub fn test(&mut self) -> Result<(), &'static str> {
+        self.command("test");
 
         Ok(())
     }
 
-    pub fn command_reboot(&mut self) -> Result<(), &'static str> {
-        self.write_command("reboot");
+    pub fn reboot(&mut self) -> Result<(), &'static str> {
+        self.command("reboot");
 
         Ok(())
     }
 
-    pub fn command_factory_reset(&mut self) -> Result<(), &'static str> {
-        self.write_command("factoryreset");
+    pub fn factory_reset(&mut self) -> Result<(), &'static str> {
+        self.command("factoryreset");
 
         Ok(())
     }
 
-    pub fn write_command(&mut self, command: &str) -> Result<&'static str, &'static str> {
+    pub fn command(&mut self, command: &str) -> Result<&'static str, &'static str> {
         self.serial.write_str("AT+").unwrap();
         self.serial.write_str(command).unwrap();
         self.serial.write_str("\r\n").unwrap();
