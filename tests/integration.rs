6bbdc3fa869695211a2c665172c875b49cd879b8
Liam Kinne <liam@kinne.io>
2023-02-08 20:58:32 +1000

    remove comments

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,16 +5,11 @@ use defmt_rtt as _;
 use panic_probe as _;
 use stm32l4xx_hal as _;
 
-// same panicking *behavior* as `panic-probe` but doesn't print a panic message
-// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
 #[defmt::panic_handler]
 fn panic() -> ! {
     cortex_m::asm::udf()
 }
 
-
-// See https://crates.io/crates/defmt-test/0.3.0 for more documentation (e.g. about the 'state'
-// feature)
 #[defmt_test::tests]
 mod tests {
     use defmt::assert;
