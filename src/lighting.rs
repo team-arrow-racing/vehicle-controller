cf884fd23d63bd906dea3bea2eba519df9b2c33f
Liam Kinne <liam@kinne.io>
2023-03-07 20:36:48 +1000

    implement lighting state machine

--- src/lighting.rs
+++ src/lighting.rs
@@ -2,11 +2,14 @@
 
 #![allow(dead_code)]
 
+use crate::app::monotonics::MonoTimer as monotonic;
 use bitflags::bitflags;
+use stm32l4xx_hal::gpio::{ErasedPin, Output, PushPull};
 
 bitflags! {
     /// As per
-    pub struct LampState: u8 {
+    #[derive(Default)]
+    pub struct LampsState: u8 {
         // indicator lamps
         const INDICATOR_LEFT = (0 << 0);
         const INDICATOR_RIGHT = (0 << 1);
@@ -17,5 +20,59 @@ bitflags! {
 
         // stop lamps
         const STOP = (0 << 3);
+    }
+}
+
+type OutputPin = ErasedPin<Output<PushPull>>;
+
+pub struct Lamps {
+    state: LampsState,
+    on_cycle: bool,
+}
+
+impl Lamps {
+    pub fn new() -> Self {
+        Self {
+            state: LampsState {
+                ..Default::default()
+            },
+            on_cycle: false,
+        }
+    }
+
+    pub fn set_left_indicator(&mut self, state: bool) {
+        self.state.set(LampsState::INDICATOR_LEFT, state);
+    }
+
+    pub fn set_right_indicator(&mut self, state: bool) {
+        self.state.set(LampsState::INDICATOR_RIGHT, state);
+    }
+
+    pub fn set_hazards(&mut self, state: bool) {
+        self.state.set(LampsState::HAZARD, state);
+    }
+
+    pub fn set_daytime(&mut self, state: bool) {
+        self.state.set(LampsState::DAYTIME, state);
+    }
+
+    pub fn set_stop(&mut self, state: bool) {
+        self.state.set(LampsState::STOP, state);
+    }
+
+    pub fn run(&mut self) -> LampsState {
+        let time = monotonic::now();
+
+        // 50% duty cycle waveform
+        let on = (time.duration_since_epoch().to_millis() % 1000) > 500;
+
+        let mut state = self.state;
+
+        // clear indicator states if they should be off
+        if !on {
+            state.remove(LampsState::HAZARD);
+        }
+
+        state
     }
 }
