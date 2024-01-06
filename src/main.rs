d94c97aea2dfc97310be6fea4a209f71b387b2f4
Liam Kinne <liam@kinne.io>
2024-01-06 21:41:35 +1000

    configure pll for fdcan

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,14 @@ mod app {
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
         let syscfg = cx.device.SYSCFG;
-        let pwr = cx.device.PWR.constrain().smps().vos0(&syscfg).freeze();
         let rcc = cx.device.RCC.constrain();
+
+        let pwr = cx.device.PWR.constrain().smps().vos0(&syscfg).freeze();
+
         let clocks = rcc
             .sysclk(480.MHz())
             .pll1_strategy(hal::rcc::PllConfigStrategy::Iterative)
+            .pll1_q_ck(32.MHz())
             .freeze(pwr, &syscfg);
 
         let systick_mono_token = rtic_monotonics::create_systick_token!();
